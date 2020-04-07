d74e102fbe7f66face59cd8e32a9fed7d9287983
shubham <shubham99bisht@gmail.com>
2020-04-07 08:28:10 +0530

    past challenges added

---
+++ assets/js/past.js
@@ -0,0 +1,6 @@
+function show(div_id){
+  document.getElementById("basic").style.display="none";
+  document.getElementById("intermediate").style.display="none";
+  document.getElementById("advanced").style.display="none";
+  document.getElementById(div_id).style.display="block";
+}
